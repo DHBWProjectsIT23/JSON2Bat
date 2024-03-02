5e8df88152e2e6fa40a098df3844f0723c223f80
Simon <simon21.blum@gmail.com>
2024-03-02 12:24:20 +0100

    Add LoggingWrapper with examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,65 @@
+#include <LoggingWrapper.hpp>
 #include <iostream>
 #include <jsoncpp/json.h>
-#include <LoggingWrapper.hpp>
 
 INITIALIZE_EASYLOGGINGPP
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+namespace WIP {
+void setupEasyLogging();
 }
+
+int main(int argc, char *argv[]) {
+  WIP::setupEasyLogging();
+  std::cout << "Hello, World!" << std::endl;
+
+  /** \note 
+   * Example of using the logging wrapper!
+   * For some cases, I thought would be most common, there are
+   * definitions/macros (OUTPUT, LOG_INFO, LOG_WARNING, LOG_ERROR, LOG_DEBUG)
+   * that can be used. For other cases, the utils::log function can be used
+   * directly. Possibilitys:
+   * - utils::log(bool verbose = false) - Logs INFO, verbose can be set to true
+   *
+   * - utils::log(LogLevel level) - Logs the given level, verbose is false,
+   * however only INFO can be verbose anyway, as all other always print to the
+   * console
+   *
+   * - utils::log(comst std::string &prefix, bool verbose = false) - Logs INFO,
+   *     with a custom prefix, verbose can be set to true
+   *
+   * - utils::log(const std::string &prefix, LogLevel level, bool verbose =
+   * false) - Logs the given level, with a custom prefix, verbose can be set to
+   * true
+   *
+   * All of the above return a std::ostream, so you can use it like e.g. a normal
+   * std::cout!
+   */
+  OUTPUT << "Hello I am a normal output! I will also be logged!\n";
+  LOG_INFO
+      << "I am a log info, I won't get printed to the console automatically!\n";
+  // The true defines that it is verbose - the default level is INFO
+  utils::log(true) << "I am a log info, I will get printed to the console!\n";
+  LOG_WARNING << "I am a log warning, I will get printed to the console!\n";
+  LOG_ERROR << "I am a log error, I will get printed to the console!\n";
+  LOG_DEBUG << "I am a log debug, I will get printed to the console!\n";
+  // False is the default value and can be omitted
+  utils::log("Custom: ", false)
+      << "I am a log with a prefix, I won't get printed to the console!\n";
+  utils::log("Custom: ", true)
+      << "I am a log with a prefix, I will get printed to the console!\n";
+
+  return 0;
+}
+
+namespace WIP {
+/**
+ * \brief Setup the easylogging++ logger
+ * \todo
+ * - Should be moved to a separate file
+ * - Proper configuration of easylogging++
+ */
+void setupEasyLogging() {
+  el::Configurations conf("conf/easylogging.conf");
+  el::Loggers::reconfigureAllLoggers(conf);
+}
+} // namespace WIP
