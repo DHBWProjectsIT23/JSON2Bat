36455673cdb3db6248e3dbd345ffedb397b44600
Simon <simon21.blum@gmail.com>
2024-03-02 11:45:50 +0100

    Initialize easylogging in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <jsoncpp/json.h>
+#include <LoggingWrapper.hpp>
+
+INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello, World!" << std::endl;
