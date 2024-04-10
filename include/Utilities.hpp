8d4de9177aea08c55bd0fe023459337f07a2551a
Simon <simon21.blum@gmail.com>
2024-04-10 14:10:55 +0200

    Add function to check if file exists

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -4,5 +4,6 @@ namespace utilities {
 class Utils {
   public:
     static void setupEasyLogging(const std::string& configFile);
+    static bool checkIfFileExists(const std::string& fileName);
 };
 } // namespace utilities
