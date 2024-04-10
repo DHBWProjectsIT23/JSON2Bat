f55bd03ed41b755580bbda1eabc0513173bb2a8b
Simon <simon21.blum@gmail.com>
2024-04-10 14:11:01 +0200

    Add function to check file ending

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -5,5 +5,6 @@ class Utils {
   public:
     static void setupEasyLogging(const std::string& configFile);
     static bool checkIfFileExists(const std::string& fileName);
+    static bool checkFileEnding(const std::string& fileName);
 };
 } // namespace utilities
