5ed940e96447cf1f597367bb22f646766ced0937
Simon <simon21.blum@gmail.com>
2024-04-10 14:11:12 +0200

    Add continue check

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -6,5 +6,7 @@ class Utils {
     static void setupEasyLogging(const std::string& configFile);
     static bool checkIfFileExists(const std::string& fileName);
     static bool checkFileEnding(const std::string& fileName);
+    static bool
+    askToContinue(const std::string& prompt = "Do you want to continue? (Y/N)\n");
 };
 } // namespace utilities
