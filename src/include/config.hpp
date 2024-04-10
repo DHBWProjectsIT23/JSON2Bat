428ed5d8fce2d1f146ddee6d2e6619191213383d
Simon <simon21.blum@gmail.com>
2024-04-10 14:17:28 +0200

    Update Version

--- src/include/config.hpp
+++ src/include/config.hpp
@@ -3,7 +3,7 @@
 #define CONFIG_HPP
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
-#define PATCH_VERSION 3
+#define PATCH_VERSION 4
 #define DESCRIPTION "A simple tool to convert json to batch."
 #define PROJECT_NAME "jsonToBatch"
 #define AUTHORS "Elena Schwarzbach, Max Rodler, Simon Blum, Sonia Sinaci"
