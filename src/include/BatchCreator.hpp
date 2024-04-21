fc9a576d6c3571512c3c2e8b0d5743f4a965cd42
MaxRodler <maximilianreinerrodler@gmail.com>
2024-04-21 22:32:51 +0200

    batchCreator0.5

--- src/include/BatchCreator.hpp
+++ src/include/BatchCreator.hpp
@@ -10,4 +10,24 @@
  * @license GNU GPLv3
  * @copyright See LICENSE file
  */
+#include "FileData.hpp"
+#include "JsonHandler.hpp"
 
+class BatchCreator {
+public:
+
+    BatchCreator(std::shared_ptr<parsing::FileData> fileData);
+
+    std::string batchFileString;
+    std::shared_ptr<parsing::FileData> fileData;
+    void writeBatch();
+
+private:
+
+    void writeStart();
+
+    void writeHideShell();
+
+    void writeCommand();
+ 
+};
