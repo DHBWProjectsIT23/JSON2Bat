923230574adf6e86d19507b0936142aa3dba93a8
MaxRodler <maximilianreinerrodler@gmail.com>
2024-04-22 11:31:57 +0200

    lost

--- src/include/BatchCreator.hpp
+++ src/include/BatchCreator.hpp
@@ -1,7 +1,7 @@
 /**
  * @file BatchCreator.hpp
- * @author
- * @date
+ * @author Maximilian Rodler
+ * @date 22.04.2024
  * @version
  * @brief
  * @details
@@ -11,32 +11,90 @@
 
 #include "FileData.hpp"
 #include "JsonHandler.hpp"
+#include <fstream>
 
+/**
+ * @class BatchCreator
+ * @brief Erstellt Batch Datei
+ * @details Wandelt Elemente aus JSON-Datei in Batch-Format um
+ *
+ * @see
+ *
+ */
 class BatchCreator {
 public:
 
+    /**
+     * @brief Initialisiert BatchCreator
+     * @details
+     * 
+     * @param filename
+     *
+     */
     BatchCreator(std::shared_ptr<parsing::FileData> fileData);
 
     std::shared_ptr<parsing::FileData> fileData;
 
-    std::string batchFileString; // Noch in Datei schreiben (shared_ptr?)
-    void writeBatch(); // Datei als Rückgabewert?
+    std::ofstream batchFile;
+    void createBatch();
 
 private:
 
+    /**
+     * @brief Anfang der Batch Datei
+     * @details Schreibt den Teil der Batch Datei der immer gleich ist.
+     * - setzt ECHO off
+     * - startet cmd.exe
+     * 
+     */
     void writeStart();
 
+    /**
+     * @brief Sichtbarkeit Konsole
+     * @details Zeigt bzw. versteckt Konsolenausgabe
+     *
+     */
     void writeHideShell();
-  
+
+    /**
+     * @brief Befehle ausführen
+     * @details Führt Befehle aus:
+     * Zu finden unter "EXE" als "command"
+     *
+     */
     void writeCommands();
 
+    /**
+     * @brief Umgebungsvariablen setzten
+     * @details Setzt Umgebungsvariablen aus "ENV" nach folgender Syntax:
+     * Eintrag unter "key" = Eintrag unter "value"
+     *
+     */
     void writeEnvVariables();
 
+    /**
+     * @brief Pfade setzten
+     * @details Verknüpft die unter "PATH" angegebenen Pfade mit dem Systempfad
+     * Setzt Pfad
+     *
+     */
     void writePathVariables();
 
-    void writeApp(); // start "WAS"? Das gleiche wie in "outputfile" ohne ".bat"
+    /**
+     * @brief Öffnet Anwednung falls gewünscht
+     * @details Öffnet Anwedung, falls unter "application" gegeben
+     * Gibt den Namen ????
+     *
+     */
+    void writeApp(); // @todo start:das gleiche wie in "outputfile" ohne ".bat"
 
-    void writeEnd(); // brauch ich noch irgendwo n carriage return? wenn aj wo?
+    /**
+     * @brief Ende der Batch Datei
+     * @details Schreibt den teil der Batch Datei der immer gelich ist
+     * - setzt ECHO OFF
+     *
+     */
+    void writeEnd(); // brauch ich noch irgendwo einen carriage return? wenn ja wo?
 
-
+ 
 };
