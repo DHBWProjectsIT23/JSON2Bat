abd3db0a6e1e31f7d518e2462be976ff112b9a15
DefinitelyNotSimon13 <DefinitelyNotSimon13@users.noreply.github.com>
2024-04-25 12:27:49 +0000

    deploy: b11e5e61f3d2ed6423b79641a95da87d6d063f90

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['parsearguments_0',['parseArguments',['../d4/d06/classcli_1_1CommandLineHandler.html#acb2f456a899bdbf41772b1ccf03d4ca8',1,'cli::CommandLineHandler']]],
   ['parsefile_1',['parseFile',['../d5/da0/classparsing_1_1JsonHandler.html#a9a56ef8d2c4f19e714e61b5bf33cf5bd',1,'parsing::JsonHandler']]],
-  ['parsefiles_2',['parseFiles',['../df/d0a/main_8cpp.html#a25bb3f0f5080fe925eab53eefc7c0e6f',1,'main.cpp']]],
+  ['parsefiles_2',['parseFiles',['../df/d0a/main_8cpp.html#aefaae33c8deff99024fbb1f547364e08',1,'main.cpp']]],
   ['parsing_3',['parsing',['../d5/da5/namespaceparsing.html',1,'']]],
   ['parsingexception_4',['parsingexception',['../dc/de6/classexceptions_1_1ParsingException.html',1,'exceptions::ParsingException'],['../dc/de6/classexceptions_1_1ParsingException.html#ac3b7cce2cab3371b18e3b834b0b83aad',1,'exceptions::ParsingException::ParsingException()']]],
   ['patch_5fversion_5',['PATCH_VERSION',['../d8/dfd/config_8hpp.html#aed5f6b4b087292b1f6579952fc7c14eb',1,'config.hpp']]],
--- html/search/functions_9.js
+++ html/search/functions_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['parsearguments_0',['parseArguments',['../d4/d06/classcli_1_1CommandLineHandler.html#acb2f456a899bdbf41772b1ccf03d4ca8',1,'cli::CommandLineHandler']]],
   ['parsefile_1',['parseFile',['../d5/da0/classparsing_1_1JsonHandler.html#a9a56ef8d2c4f19e714e61b5bf33cf5bd',1,'parsing::JsonHandler']]],
-  ['parsefiles_2',['parseFiles',['../df/d0a/main_8cpp.html#a25bb3f0f5080fe925eab53eefc7c0e6f',1,'main.cpp']]],
+  ['parsefiles_2',['parseFiles',['../df/d0a/main_8cpp.html#aefaae33c8deff99024fbb1f547364e08',1,'main.cpp']]],
   ['parsingexception_3',['ParsingException',['../dc/de6/classexceptions_1_1ParsingException.html#ac3b7cce2cab3371b18e3b834b0b83aad',1,'exceptions::ParsingException']]],
   ['printcredits_4',['printCredits',['../d4/d06/classcli_1_1CommandLineHandler.html#ac1613613eb4f4534ee55aba547a2a470',1,'cli::CommandLineHandler']]],
   ['printhelp_5',['printHelp',['../d4/d06/classcli_1_1CommandLineHandler.html#ac81856dd50394363675207068213d960',1,'cli::CommandLineHandler']]],
