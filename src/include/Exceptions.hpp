2acffe2fc5565c9e5ed9ce33bb048f9265e290da
Simon <simon21.blum@gmail.com>
2024-04-26 18:09:20 +0200

    Add Character exception

--- src/include/Exceptions.hpp
+++ src/include/Exceptions.hpp
@@ -34,9 +34,9 @@ namespace exceptions {
  */
 class CustomException : public std::exception {
 public:
-    [[nodiscard]] const char* what() const noexcept override {
-        return "Base Exception";
-    }
+  [[nodiscard]] const char *what() const noexcept override {
+    return "Base Exception";
+  }
 };
 
 /**
@@ -45,26 +45,26 @@ public:
  */
 class ParsingException : public CustomException {
 private:
-    const std::string file;
-    std::string message;
+  const std::string file;
+  std::string message;
 
 public:
-    explicit ParsingException(const std::string &file) : file(file) {
-        /**
-         * @note I planned to use std::format, however it seems that the
-         * required Compiler Version is not yet available in the stable Ubuntu
-         * Repo!
-         */
-        std::stringstream ss;
-        ss << "Error while trying to parse \"" << file << "\"!\n"
-           << "There most likely is a syntax error within the \".json\" file.";
-        this->message = ss.str();
-        LOG_INFO << "ParsingException: " << message;
-    }
+  explicit ParsingException(const std::string &file) : file(file) {
+    /**
+     * @note I planned to use std::format, however it seems that the
+     * required Compiler Version is not yet available in the stable Ubuntu
+     * Repo!
+     */
+    std::stringstream ss;
+    ss << "Error while trying to parse \"" << file << "\"!\n"
+       << "There most likely is a syntax error within the \".json\" file.";
+    this->message = ss.str();
+    LOG_INFO << "ParsingException: " << message;
+  }
 
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -73,25 +73,25 @@ public:
  */
 class FileExistsException : public CustomException {
 private:
-    const std::string file;
-    std::string message;
+  const std::string file;
+  std::string message;
 
 public:
-    explicit FileExistsException(const std::string &file) : file(file) {
-        /**
-         * @note I planned to use std::format, however it seems that the
-         * required Compiler Version is not yet available in the stable Ubuntu
-         * Repo!
-         */
-        std::stringstream ss;
-        ss << "The outputfile \"" << file << "\" already exists!";
-        this->message = ss.str();
-        LOG_INFO << "BatchExistsException: " << message;
-    }
+  explicit FileExistsException(const std::string &file) : file(file) {
+    /**
+     * @note I planned to use std::format, however it seems that the
+     * required Compiler Version is not yet available in the stable Ubuntu
+     * Repo!
+     */
+    std::stringstream ss;
+    ss << "The outputfile \"" << file << "\" already exists!";
+    this->message = ss.str();
+    LOG_INFO << "BatchExistsException: " << message;
+  }
 
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -100,25 +100,25 @@ public:
  */
 class InvalidValueException : public CustomException {
 private:
-    const std::string key;
-    std::string message;
+  const std::string key;
+  std::string message;
 
 public:
-    InvalidValueException(const std::string &key, const std::string &issue)
-        : key(key) {
-        /**
-         * @note I planned to use std::format, however it seems that the
-         * required Compiler Version is not yet available in the stable Ubuntu
-         * Repo!
-         */
-        std::stringstream ss;
-        ss << "Error at key \"" << key << "\"! " << issue;
-        this->message = ss.str();
-        LOG_INFO << "InvalidValueException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  InvalidValueException(const std::string &key, const std::string &issue)
+      : key(key) {
+    /**
+     * @note I planned to use std::format, however it seems that the
+     * required Compiler Version is not yet available in the stable Ubuntu
+     * Repo!
+     */
+    std::stringstream ss;
+    ss << "Error at key \"" << key << "\"! " << issue;
+    this->message = ss.str();
+    LOG_INFO << "InvalidValueException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -134,21 +134,21 @@ public:
  */
 class InvalidKeyException : public CustomException {
 private:
-    std::string message = "Invalid key found!";
+  std::string message = "Invalid key found!";
 
 public:
-    explicit InvalidKeyException(
-        const std::vector<std::tuple<int, std::string>> &keys) {
-        LOG_INFO << "InvalidKeyException: " << message;
+  explicit InvalidKeyException(
+      const std::vector<std::tuple<int, std::string>> &keys) {
+    LOG_INFO << "InvalidKeyException: " << message;
 
-        for (const auto &[line, key] : keys) {
-            LOG_WARNING << "Invalid key found at line " << line << ": \"" << key
-                        << "\"!";
-        }
+    for (const auto &[line, key] : keys) {
+      LOG_WARNING << "Invalid key found at line " << line << ": \"" << key
+                  << "\"!";
     }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -161,24 +161,24 @@ public:
  */
 class InvalidTypeException : public CustomException {
 private:
-    const std::string type;
-    std::string message;
+  const std::string type;
+  std::string message;
 
 public:
-    InvalidTypeException(const std::string &type, int line) : type(type) {
-        /**
-         * @note I planned to use std::format, however it seems that the
-         * required Compiler Version is not yet available in the stable Ubuntu
-         * Repo!
-         */
-        std::stringstream ss;
-        ss << "Invalid type found at line " << line << ": \"" << type << "\"";
-        this->message = ss.str();
-        LOG_INFO << "InvalidTypeException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  InvalidTypeException(const std::string &type, int line) : type(type) {
+    /**
+     * @note I planned to use std::format, however it seems that the
+     * required Compiler Version is not yet available in the stable Ubuntu
+     * Repo!
+     */
+    std::stringstream ss;
+    ss << "Invalid type found at line " << line << ": \"" << type << "\"";
+    this->message = ss.str();
+    LOG_INFO << "InvalidTypeException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -190,26 +190,26 @@ public:
  */
 class MissingKeyException : public CustomException {
 private:
-    std::string message;
-    std::string type;
-    std::string key;
+  std::string message;
+  std::string type;
+  std::string key;
 
 public:
-    MissingKeyException(const std::string &key, const std::string &type)
-        : type(type), key(key) {
-        /**
-         * @note I planned to use std::format, however it seems that the
-         * required Compiler Version is not yet available in the stable Ubuntu
-         * Repo!
-         */
-        std::stringstream ss;
-        ss << "Missing key \"" << key << "\" for type \"" << type << "\"!";
-        this->message = ss.str();
-        LOG_INFO << "MissingKeyException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  MissingKeyException(const std::string &key, const std::string &type)
+      : type(type), key(key) {
+    /**
+     * @note I planned to use std::format, however it seems that the
+     * required Compiler Version is not yet available in the stable Ubuntu
+     * Repo!
+     */
+    std::stringstream ss;
+    ss << "Missing key \"" << key << "\" for type \"" << type << "\"!";
+    this->message = ss.str();
+    LOG_INFO << "MissingKeyException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -220,15 +220,13 @@ public:
  */
 class MissingTypeException : public CustomException {
 private:
-    std::string message = "Missing \"type\" key for at least one entry!";
+  std::string message = "Missing \"type\" key for at least one entry!";
 
 public:
-    MissingTypeException() {
-        LOG_INFO << "MissingTypeException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  MissingTypeException() { LOG_INFO << "MissingTypeException: " << message; }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -237,19 +235,19 @@ public:
  */
 class UnreachableCodeException : public CustomException {
 private:
-    std::string message;
+  std::string message;
 
 public:
-    explicit UnreachableCodeException(const std::string &message)
-        : message(message) {
-        OUTPUT << "This exception happened due to a bug in the application!\n"
-               << "Please report this bug! See " << config::EXECUTABLE_NAME
-               << " -c for contact information.\n";
-        LOG_INFO << "UnreachableCodeException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  explicit UnreachableCodeException(const std::string &message)
+      : message(message) {
+    OUTPUT << "This exception happened due to a bug in the application!\n"
+           << "Please report this bug! See " << config::EXECUTABLE_NAME
+           << " -c for contact information.\n";
+    LOG_INFO << "UnreachableCodeException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -258,16 +256,16 @@ public:
  */
 class FailedToOpenFileException : public CustomException {
 private:
-    std::string message;
+  std::string message;
 
 public:
-    explicit FailedToOpenFileException(const std::string &file) {
-        message = "Failed to open file: " + file;
-        LOG_INFO << "FailedToOpenFileException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  explicit FailedToOpenFileException(const std::string &file) {
+    message = "Failed to open file: " + file;
+    LOG_INFO << "FailedToOpenFileException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 /**
@@ -276,16 +274,34 @@ public:
  */
 class NoSuchDirException : public CustomException {
 private:
-    std::string message;
+  std::string message;
 
 public:
-    explicit NoSuchDirException(const std::string &dir) {
-        message = "No such directory: " + dir;
-        LOG_INFO << "NoSuchDirException: " << message;
-    }
-    [[nodiscard]] const char* what() const noexcept override {
-        return message.c_str();
-    }
+  explicit NoSuchDirException(const std::string &dir) {
+    message = "No such directory: " + dir;
+    LOG_INFO << "NoSuchDirException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
+};
+
+/**
+ * @class ContainsBadCharacterException
+ * @brief Exception for when a string contains bad characters
+ */
+class ContainsBadCharacterException : public CustomException {
+private:
+  std::string message;
+
+public:
+  explicit ContainsBadCharacterException(const std::string &value) {
+    message = "The value \"" + value + "\" contains bad characters!";
+    LOG_INFO << "ContainsBadCharacterException: " << message;
+  }
+  [[nodiscard]] const char *what() const noexcept override {
+    return message.c_str();
+  }
 };
 
 } // namespace exceptions
--- src/sources/JsonHandler.cpp
+++ src/sources/JsonHandler.cpp
@@ -16,110 +16,143 @@
 #include "KeyValidator.hpp"
 #include "LoggingWrapper.hpp"
 
+#include <algorithm>
+
 namespace parsing {
 JsonHandler::JsonHandler(const std::string &filename) {
-    LOG_INFO << "Initializing JSONHandler with filename: " << filename << "\n";
-    this->root = parseFile(filename);
+  LOG_INFO << "Initializing JSONHandler with filename: " << filename << "\n";
+  this->root = parseFile(filename);
 }
 
 std::shared_ptr<Json::Value> JsonHandler::parseFile(const std::string &filename)
 
 {
-    LOG_INFO << "Parsing file: " << filename << "\n";
-    // Can open files anywhere with relative/absolute path
-    // - {ReqFunc5}
-    std::ifstream file(filename);
-    Json::Value newRoot;
+  LOG_INFO << "Parsing file: " << filename << "\n";
+  // Can open files anywhere with relative/absolute path
+  // - {ReqFunc5}
+  std::ifstream file(filename);
+  Json::Value newRoot;
 
-    // Json::Reader.parse() returns false if parsing fails
-    if (Json::Reader reader; !reader.parse(file, newRoot)) {
-        throw exceptions::ParsingException(filename);
-    }
+  // Json::Reader.parse() returns false if parsing fails
+  if (Json::Reader reader; !reader.parse(file, newRoot)) {
+    throw exceptions::ParsingException(filename);
+  }
 
-    // Validate keys
-    // Check for errors
-    if (auto errors = KeyValidator::getInstance().validateKeys(newRoot, filename);
-            !errors.empty()) {
-        throw exceptions::InvalidKeyException(errors);
-    }
+  // Validate keys
+  // Check for errors
+  if (auto errors = KeyValidator::getInstance().validateKeys(newRoot, filename);
+      !errors.empty()) {
+    throw exceptions::InvalidKeyException(errors);
+  }
 
-    LOG_INFO << "File \"" << filename << "\" has been parsed\n";
-    return std::make_shared<Json::Value>(newRoot);
+  LOG_INFO << "File \"" << filename << "\" has been parsed\n";
+  return std::make_shared<Json::Value>(newRoot);
 }
 
 std::shared_ptr<FileData> JsonHandler::getFileData() {
-    LOG_INFO << "Creating FileData object for return...\n";
-    return this->createFileData();
+  LOG_INFO << "Creating FileData object for return...\n";
+  return this->createFileData();
 }
 
 std::shared_ptr<FileData> JsonHandler::createFileData() {
-    LOG_INFO << "Creating FileData object...\n";
-    this->data = std::make_shared<FileData>();
-    this->assignOutputFile();
-    this->assignHideShell();
-    this->assignApplication();
-    this->assignEntries();
-    return this->data;
+  LOG_INFO << "Creating FileData object...\n";
+  this->data = std::make_shared<FileData>();
+  this->assignOutputFile();
+  this->assignHideShell();
+  this->assignApplication();
+  this->assignEntries();
+  return this->data;
 }
 
 void JsonHandler::assignOutputFile() const {
-    LOG_INFO << "Assigning outputfile...\n";
-    std::string outputFile = this->root->get("outputfile", "").asString();
-    this->data->setOutputFile(outputFile);
+  LOG_INFO << "Assigning outputfile...\n";
+  std::string outputFile = this->root->get("outputfile", "").asString();
+  if (containsBadCharacter(outputFile)) {
+    throw exceptions::ContainsBadCharacterException(outputFile);
+  }
+  this->data->setOutputFile(outputFile);
 }
 
 void JsonHandler::assignHideShell() const {
-    LOG_INFO << "Assigning hide shell...\n";
-    // If the 'hideshell' key is not given, it defaults to false
-    this->data->setHideShell(this->root->get("hideshell", false).asBool());
+  LOG_INFO << "Assigning hide shell...\n";
+  // If the 'hideshell' key is not given, it defaults to false
+  this->data->setHideShell(this->root->get("hideshell", false).asBool());
 }
 
 void JsonHandler::assignApplication() const {
-    LOG_INFO << "Assigning application...\n";
-    this->data->setApplication(this->root->get("application", "").asString());
+  LOG_INFO << "Assigning application...\n";
+  std::string application = this->root->get("application", "").asString();
+  if (containsBadCharacter(application)) {
+    throw exceptions::ContainsBadCharacterException(application);
+  }
+  this->data->setApplication(application);
 }
 
 void JsonHandler::assignEntries() const {
-    LOG_INFO << "Assigning entries...\n";
+  LOG_INFO << "Assigning entries...\n";
 
-    for (const auto &entry : this->root->get("entries", "")) {
-        std::string entryType = entry.get("type", "").asString();
+  for (const auto &entry : this->root->get("entries", "")) {
+    std::string entryType = entry.get("type", "").asString();
 
-        if (entryType == "EXE") {
-            LOG_INFO << "Calling function to assign command...\n";
-            this->assignCommand(entry);
-        }
-        else if (entryType == "ENV") {
-            LOG_INFO << "Calling function to assign environment variable...\n";
-            this->assignEnvironmentVariable(entry);
-        }
-        else if (entryType == "PATH") {
-            LOG_INFO << "Calling function to assign path value...\n";
-            this->assignPathValue(entry);
-        }
-        else {
-            // Due to validation beforehand - this should never be reached!
-            throw exceptions::UnreachableCodeException(
-                "Unknown entries should be caught by KeyValidator!\nPlease report "
-                "this bug!");
-        }
+    if (entryType == "EXE") {
+      LOG_INFO << "Calling function to assign command...\n";
+      this->assignCommand(entry);
+    } else if (entryType == "ENV") {
+      LOG_INFO << "Calling function to assign environment variable...\n";
+      this->assignEnvironmentVariable(entry);
+    } else if (entryType == "PATH") {
+      LOG_INFO << "Calling function to assign path value...\n";
+      this->assignPathValue(entry);
+    } else {
+      // Due to validation beforehand - this should never be reached!
+      throw exceptions::UnreachableCodeException(
+          "Unknown entries should be caught by KeyValidator!\nPlease report "
+          "this bug!");
     }
+  }
 }
 
 void JsonHandler::assignCommand(const Json::Value &entry) const {
-    LOG_INFO << "Assigning command...\n";
-    this->data->addCommand(entry.get("command", "").asString());
+  LOG_INFO << "Assigning command...\n";
+  std::string command = entry.get("command", "").asString();
+  if (containsBadCharacter(command)) {
+    throw exceptions::ContainsBadCharacterException(command);
+  }
+  this->data->addCommand(command);
 }
 
 void JsonHandler::assignEnvironmentVariable(const Json::Value &entry) const {
-    LOG_INFO << "Assigning environment variable...\n";
-    std::string key = entry.get("key", "").asString();
-    std::string value = entry.get("value", "").asString();
-    this->data->addEnvironmentVariable(key, value);
+  LOG_INFO << "Assigning environment variable...\n";
+  std::string key = entry.get("key", "").asString();
+  std::string value = entry.get("value", "").asString();
+
+  if (containsBadCharacter(key) || containsBadCharacter(value)) {
+    throw exceptions::ContainsBadCharacterException(key + " " + value);
+  }
+  this->data->addEnvironmentVariable(key, value);
 }
 
 void JsonHandler::assignPathValue(const Json::Value &entry) const {
-    LOG_INFO << "Assigning path value...\n";
-    this->data->addPathValue(entry.get("path", "").asString());
+  LOG_INFO << "Assigning path value...\n";
+  std::string path = entry.get("path", "").asString();
+  if (containsBadCharacter(path)) {
+    throw exceptions::ContainsBadCharacterException(path);
+  }
+  this->data->addPathValue(path);
+}
+
+bool JsonHandler::containsBadCharacter(const std::string &str) {
+
+  static const std::unordered_set<char> badChars = {
+      '\n', '\t', '\r', '\0', '\x1A',      // Control characters
+      '&',  '|',  '<',  '>',  '(',    ')', // Special characters
+      '%',  '!',  '^',  ':',  '@',    '"'  // Batch file specific characters
+  };
+
+  auto isBadCharacter = [](char c) {
+    return badChars.find(c) != badChars.end();
+  };
+
+  return std::any_of(str.begin(), str.end(), isBadCharacter);
 }
 } // namespace parsing
