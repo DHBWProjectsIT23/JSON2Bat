2572563dd6c1670540e9d1822134b7984835fc4e
DefinitelyNotSimon13 <DefinitelyNotSimon13@users.noreply.github.com>
2024-04-26 13:59:58 +0000

    deploy: 033c93e77ef2df382824fede37531515c68c001e

--- html/da/d1c/classBatchCreator.js
+++ html/da/d1c/classBatchCreator.js
@@ -1,9 +1,9 @@
 var classBatchCreator =
 [
     [ "BatchCreator", "da/d1c/classBatchCreator.html#ad2fe34f5fef34f26763558bc9c9949a3", null ],
-    [ "createBatch", "da/d1c/classBatchCreator.html#a2225babf16a473ba6722238c19d785ae", null ],
+    [ "createBatch", "da/d1c/classBatchCreator.html#aca15cd9ed3a90d1f43ac2fd16175c676", null ],
     [ "getDataStream", "da/d1c/classBatchCreator.html#abf3164bb6eedfb7062c45ddf445e5965", null ],
-    [ "writeApplication", "da/d1c/classBatchCreator.html#ab0c64cf4f8d3bf824bff2762a125315d", null ],
+    [ "writeApp", "da/d1c/classBatchCreator.html#af9bfc052ff3ee90beff0f35b56aed5bc", null ],
     [ "writeCommands", "da/d1c/classBatchCreator.html#a7ea2646254825e194f831d98ae703ec8", null ],
     [ "writeEnd", "da/d1c/classBatchCreator.html#a8d7aab76f68225bc13cdcb41035f5319", null ],
     [ "writeEnvVariables", "da/d1c/classBatchCreator.html#a31d22f486b3608cdae0b7488fb3857a8", null ],
