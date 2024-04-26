697dde3f410bc0b0a00e1d134944329c2f00dda4
DefinitelyNotSimon13 <DefinitelyNotSimon13@users.noreply.github.com>
2024-04-26 14:04:54 +0000

    deploy: 4fc0b9ba435ee0407a487e7b0665fa34cd859116

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['options_0',['options',['../d1/d38/structoptions.html',1,'options'],['../d5/dfa/namespacecli.html#a28a7c34e9990a8cc28324ac926bb9be5',1,'cli::options']]],
-  ['other_1',['Other',['../index.html#autotoc_md6',1,'']]],
+  ['of_20contents_0',['Table of Contents',['../index.html#autotoc_md1',1,'']]],
+  ['options_1',['options',['../d1/d38/structoptions.html',1,'options'],['../d5/dfa/namespacecli.html#a28a7c34e9990a8cc28324ac926bb9be5',1,'cli::options']]],
   ['outputfile_2',['outputfile',['../da/d06/classparsing_1_1FileData.html#a512ec5915338a9f8e1884bc5dc543abf',1,'parsing::FileData']]]
 ];
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -3,6 +3,6 @@ var searchData=
   ['checkconfigfile_0',['checkConfigFile',['../d6/d9e/classutilities_1_1Utils.html#abf18cb6fd599bec6e8c55b62ee178640',1,'utilities::Utils']]],
   ['checkdirectory_1',['checkDirectory',['../d6/d9e/classutilities_1_1Utils.html#ac07ca745d24efb547beec94f1d6acc11',1,'utilities::Utils']]],
   ['commandlinehandler_2',['CommandLineHandler',['../d4/d06/classcli_1_1CommandLineHandler.html#af35432870c7365cebcdf348d74f4bbb2',1,'cli::CommandLineHandler']]],
-  ['createbatch_3',['createBatch',['../da/d1c/classBatchCreator.html#a2225babf16a473ba6722238c19d785ae',1,'BatchCreator']]],
+  ['createbatch_3',['createBatch',['../da/d1c/classBatchCreator.html#aca15cd9ed3a90d1f43ac2fd16175c676',1,'BatchCreator']]],
   ['createfiledata_4',['createFileData',['../d5/da0/classparsing_1_1JsonHandler.html#a9e765c81ea918e5eb84811ac929d8f24',1,'parsing::JsonHandler']]]
 ];
