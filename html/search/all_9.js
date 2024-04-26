2665091de05b912cbcfc8ae5180ed2dfe2ff2173
DefinitelyNotSimon13 <DefinitelyNotSimon13@users.noreply.github.com>
2024-04-26 14:10:01 +0000

    deploy: 9317cacbfe7f67446cbe947e65604d6ddc65be88

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['json2batch_0',['JSON2Batch',['../index.html',1,'']]],
+  ['json2batch_0',['json2batch',['../index.html',1,'JSON2Batch'],['../index.html#autotoc_md0',1,'JSON2Batch']]],
   ['jsoncpp_1',['jsoncpp',['../index.html#autotoc_md13',1,'']]],
   ['jsonhandler_2',['jsonhandler',['../d5/da0/classparsing_1_1JsonHandler.html',1,'parsing::JsonHandler'],['../d5/da0/classparsing_1_1JsonHandler.html#aebaf4ed7a0dcca8082a648e2ef17f367',1,'parsing::JsonHandler::JsonHandler()'],['../d5/da0/classparsing_1_1JsonHandler.html#a678af0a2e0668f9e47b14dfdb4d954e4',1,'parsing::JsonHandler::JsonHandler(const std::string &amp;filename)']]],
   ['jsonhandler_2ecpp_3',['JsonHandler.cpp',['../d0/d2b/JsonHandler_8cpp.html',1,'']]],
